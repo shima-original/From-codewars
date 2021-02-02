823ec0feb9c0bf2208c3434007cbe7e3d7f53579
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-02 15:59:36 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -14,3 +14,23 @@ function fixTheMeerkat(arr) {
    newArr.push(item); 
  } return newArr;
 }
+
+
+/*    It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+Build a function that takes in two arguments (salary, bonus). Salary will be an integer, and bonus a boolean.
+If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+Return the total figure the individual will receive as a string prefixed with "£"   */
+
+function bonusTime(salary, bonus) {
+  let result = 0;
+  
+  switch(bonus) {
+    case 'true':
+      result = "£" + salary*10;    
+      break;
+    case 'false':
+      result = "£" + salary;    
+      break;
+  } 
+  return result;  
+}
