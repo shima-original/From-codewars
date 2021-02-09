f5106d3d797c728b51e3acbb15c036612d939d32
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-09 22:11:47 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -26,11 +26,9 @@ return (bonus === true ? '£' + salary*10 : '£' + salary);
 }
 
 
-/*It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
-
-Return the average of the given array rounded down to its nearest integer.
-
-The array will never be empty   */
+/*It's the academic year's end, fateful moment of your school report. The averages must be calculated. 
+All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+Return the average of the given array rounded down to its nearest integer.The array will never be empty   */
 
 function getAverage(marks){
   let count = 0;
