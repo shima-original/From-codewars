4ab1ed19b25bab0bc1a6afbb49bad2853dff5cc8
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-10 01:20:32 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -39,3 +39,6 @@ function getAverage(marks){
 }
 
 
+/*This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.  */
+
+const solve = (x, y) => x / y
