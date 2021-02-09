e8f292cf51d0beba944dfe70e699f4c18520cd10
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-10 01:45:47 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -41,4 +41,18 @@ function getAverage(marks){
 
 /*This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.  */
 
-const solve = (x, y) => x / y
+const solve = (x, y) => x / y;
+
+
+/*In this simple exercise, you will create a program that will take two lists of integers, a and b. 
+Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. 
+You must find the difference of the cuboids' volumes regardless of which is bigger.
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. 
+Therefore, the function should return 8.Your function will be tested with pre-made examples as well as random ones.
+If you can, try writing it in one line of code.  */
+
+function findDifference(a, b) {
+  let result = (a[0]*a[1]*a[2]) - (b[0]*b[1]*b[2]);
+  return result > 0 ? result : -(result);
+}
+
