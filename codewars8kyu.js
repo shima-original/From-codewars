67b37a3dc8d5a4bef8907ed55a1a31ee214f8b95
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-11 15:44:58 +0300

    Update codewars8kyu.js
    6 katas from codewars (8kyu)

--- codewars8kyu.js
+++ codewars8kyu.js
@@ -30,6 +30,7 @@ return (bonus === true ? '£' + salary*10 : '£' + salary);
 All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
 Return the average of the given array rounded down to its nearest integer.The array will never be empty   */
 
+
 function getAverage(marks){
   let count = 0;
    for (let item of marks) {
