96543f863aa02f67d7cfff427179e1c64e4b08f0
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-11 13:58:02 +0300

    Update codewars.js

--- codewars.js
+++ codewars.js
@@ -56,3 +56,17 @@ function findDifference(a, b) {
   return result > 0 ? result : -(result);
 }
 
+
+//Your task is to find the first element of an array that is not consecutive.By not consecutive we mean not exactly 1 larger than the previous element of the array.
+
+function firstNonConsecutive (arr) {
+  for(let i = 0; i < arr.length; i += 1) {
+    if(arr[i + 1] != arr[i] + 1) return(arr[i + 1]); 
+  }
+} 
+
+
+
+
+
+
