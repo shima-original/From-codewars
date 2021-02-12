7bc8c534b54360ac846b9fb8fc0fc1c20b8225d8
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-13 00:32:14 +0300

    Update codewars8kyuNext.js

--- codewars8kyuNext.js
+++ codewars8kyuNext.js
@@ -5,3 +5,16 @@ var replaceDots = function(str) {
   let result = arr.join('-');
   return result;
 }
+
+
+/*  For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; 
+and month 11 (November), is part of the fourth quarter.  */
+
+const quarterOf = (month) => {
+  if (month === 1 || month === 2 || month === 3) return 1;
+  else if (month === 4 || month === 5 || month === 6) return 2;
+  else if (month === 7 || month === 8 || month === 9) return 3;
+  else return 4; 
+}
+
+
