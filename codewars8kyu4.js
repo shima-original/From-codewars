d10e7f29dc34404f7b5f9b138875100d2b5c04d6
Olga Mkrtchyan <olia.mst@gmail.com>
2021-02-26 16:53:13 +0300

    Update codewars8kyu4.js

--- codewars8kyu4.js
+++ codewars8kyu4.js
@@ -4,4 +4,17 @@ function greet() {
   return "hello world!";
 }
 
+/*Finish the uefaEuro2016() function so it return string just like in the examples below: 
+uefaEuro2016(['Germany', 'Ukraine'],[2, 0]) // "At match Germany - Ukraine, Germany won!"*/
+function uefaEuro2016(teams, scores){
+  if (scores[0] > scores[1]) {
+     return `At match ${teams[0]} - ${teams[1]}, ${teams[0]} won!` 
+  }
+  else if (scores[0] < scores[1]) {
+    return `At match ${teams[0]} - ${teams[1]}, ${teams[1]} won!` 
+  }
+  else {
+    return `At match ${teams[0]} - ${teams[1]}, teams played draw.` 
+  }
+}
 
