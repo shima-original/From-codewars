45e4f528755cb5248bae13a5801d53aca85afa59
Olga Mkrtchyan <olia.mst@gmail.com>
2021-03-08 14:05:58 +0300

    Update codewars8kyu5.js

--- codewars8kyu5.js
+++ codewars8kyu5.js
@@ -11,13 +11,28 @@ function solution(str){
 }
 
 //Your task is to make two functions, max and min that take a array of integers list as input and outputs the largest and lowest number in that array/vector.
-
 let min = function(list){
-    list.sort((a,b) => a>b ? b:a);
+    list.sort((a,b) => a>b ? 1:-1);
     return list[0];
   };
 
 let max = function(list){
-  list.sort((a,b) => a<b ? b:a);
+  list.sort((a,b) => a<b ? 1:-1);
   return list[0];
 };
+
+//Implement a function which convert the given boolean value into its string representation.
+function booleanToString(b){
+  return b.toString();
+}
+
+//Write a function that takes the animal's name and dish as arguments and returns true or false to indicate whether the beast is allowed to bring the dish to the feast.
+function feast(beast, dish) {
+  let result = (beast.split('')[0] == dish.split('')[0] && beast.split('')[beast.length - 1] == dish.split('')[dish.length - 1]) ? true : false;
+  return result;
+}
+
+//We need a function that can transform a number into a string.
+function numberToString(num) {
+  return num.toString();
+}
