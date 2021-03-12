a71d3832e00d728a3f1903da3c5a6a120009292b
Olga Mkrtchyan <olia.mst@gmail.com>
2021-03-12 11:28:15 +0300

    Update codewars8kyu5.js

--- codewars8kyu5.js
+++ codewars8kyu5.js
@@ -36,3 +36,20 @@ function feast(beast, dish) {
 function numberToString(num) {
   return num.toString();
 }
+
+/*Simple challenge - eliminate all bugs from the supplied code so that the code runs and outputs the expected value. 
+Output should be the length of the longest word, as a number.There will only be one 'longest' word.  */
+function findLongest(str) {
+  
+  let spl = str.split(" ");
+  let longest = 0;
+  
+  for (let i = 0; i < spl.length; i+=1) {
+    let splWord = spl[i].split("");
+    if (splWord.length > longest) {
+      longest = splWord.length;
+    }
+  }
+  return longest;
+}
+
