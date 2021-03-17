c113bfffa5987c50c449b7dece536712e04a123c
Olga Mkrtchyan <olia.mst@gmail.com>
2021-03-17 12:56:16 +0300

    Update codewars8kyu7.js

--- codewars8kyu7.js
+++ codewars8kyu7.js
@@ -11,3 +11,32 @@ function mergeArrays(arr1, arr2) {
    }
   return mergedArray;
 }
+
+
+/* Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers. Return your answer as a number. */
+function sumMix(x) {
+  
+  let item = 0;
+  let result = 0;
+  
+  for(let i = 0; i < x.length; i += 1) {
+    item = Number(x[i]);
+    result += item;
+  } return result;
+}
+
+/*Wolves have been reintroduced to Great Britain. You are a sheep farmer, and are now plagued by wolves which pretend to be sheep. 
+Fortunately, you are good at spotting them.Warn the sheep in front of the wolf that it is about to be eaten. 
+Remember that you are standing at the front of the queue which is at the end of the array. 
+If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! 
+You are about to be eaten by a wolf!" where N is the sheep's position in the queue. Note: there will always be exactly one wolf in the array.*/
+function warnTheSheep(queue) {
+  
+  if (queue[queue.length - 1] == "wolf") return "Pls go away and stop eating my sheep";
+  else {
+    for (let i = 0; i < queue.length; i += 1) {
+       if (queue[i] != "wolf") continue;
+       else return (`Oi! Sheep number ${(queue.length - (i + 1))}! You are about to be eaten by a wolf!`);
+    } 
+  }
+}
