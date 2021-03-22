3b4f0ee7aeee28140fc54579ab34becb1663933d
Olga Mkrtchyan <olia.mst@gmail.com>
2021-03-22 12:06:42 +0300

    Update codewars8kyu8.js

--- codewars8kyu8.js
+++ codewars8kyu8.js
@@ -5,3 +5,16 @@ function cockroachSpeed(s) {
   let result = Math.floor (s*100000/3600);
   return result;
 }
+
+//Return the Nth Even Number
+function nthEven(n){
+  let arr = [];
+  arr[0] = 0;
+  arr[1] = 2;
+  
+  for(let index = 1; index <= n; index += 1) {
+    arr[index+1] = arr[index] + 2;
+  }
+  
+  return arr[n - 1];
+}
