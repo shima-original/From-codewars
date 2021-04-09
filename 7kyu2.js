2348e61d77e3b3beb636ba7f5eef1e949a5edd8e
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-10 01:44:45 +0300

    Update 7kyu2.js

--- 7kyu2.js
+++ 7kyu2.js
@@ -9,3 +9,10 @@ function switcheroo(x){
 }
 
 //Your task is to write a function that takes a string and return a new string with all vowels removed. Note: for this kata y isn't considered a vowel.
+function disemvowel(str) {
+  let arr = str.split('');
+  let result = arr.filter((elem) => elem != 'a' && elem != 'e' && elem != 'i' 
+                         && elem != 'o' && elem != 'u' && elem != 'A'
+                         && elem != 'E' && elem != 'I' && elem != 'O' && elem != 'U');
+  return result.join('');
+}
