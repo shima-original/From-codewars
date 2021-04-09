507b8abe08976a71898187dd49983d021729d40b
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-10 01:19:25 +0300

    Create 7kyu2.js

---
+++ 7kyu2.js
@@ -0,0 +1,11 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched. 
+function switcheroo(x){
+  let arr = x.split('');
+  for(let i = 0; i < arr.length; i += 1) {
+    if(arr[i] == 'a') arr[i] = 'b';
+    else if(arr[i] == 'b') arr[i] = 'a';
+  }
+    return arr.join('');
+}
+
+//Your task is to write a function that takes a string and return a new string with all vowels removed. Note: for this kata y isn't considered a vowel.
