2fc2164fc6970bec1629cfb51c857996b8870b6a
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-13 12:16:20 +0300

    Update 7kyu2.js

--- 7kyu2.js
+++ 7kyu2.js
@@ -71,6 +71,22 @@ function addArrays(array1, array2) {
   return result;
 }
 
+/*You will be given a string (x) featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'.
+You need to find out if the cat can catch the mouse from it's current position. The cat can jump over three characters. So:
+C.....m returns 'Escaped!' <-- more than three characters between
+C...m returns 'Caught!' <-- as there are three characters between the two, the cat can jump.  */
+function catMouse(x){
+  let start = 0;
+  let end = 0;
+  let result = '';
+  let arr = x.split('');
+  for(let i = 0; i < arr.length; i += 1) {
+    if(arr[i] == 'C') start = i;
+    else if(arr[i] == 'm') end = i;
+  }
+  result = (end - start) <= 4 ? 'Caught!' : 'Escaped!';
+  return result;
+}
 
 
 
