9d84e3b6cdd79e605ed6f891dea55b31fcf88e2c
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-16 11:24:05 +0300

    Update 7kyu3.js

--- 7kyu3.js
+++ 7kyu3.js
@@ -1,1 +1,9 @@
 
+//Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number. 
+function solution(value){
+  let arr = value.toString().split('');
+  while(arr.length < 5) {
+    arr.unshift('0');
+  }
+  return `Value is ${arr.join('')}`;
+}
