792aef006f39127723cd778aa284eb160f1c4cd5
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-19 16:54:34 +0300

    Update 7kyu3.js

--- 7kyu3.js
+++ 7kyu3.js
@@ -8,3 +8,12 @@ function solution(value){
   return `Value is ${arr.join('')}`;
 }
 
+/*You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. 
+If they are, return true. Else, return false.You can assume all values in the array are numbers. */
+function smallEnough(a, limit){
+  let result = true;
+  a.map(function(elem){
+    if(elem > limit) result = false;
+  });
+  return result;
+}
