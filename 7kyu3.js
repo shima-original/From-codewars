2822f3f84579b86ab72b2390615c1ee67d5f900c
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-21 11:37:33 +0300

    Update 7kyu3.js

--- 7kyu3.js
+++ 7kyu3.js
@@ -31,3 +31,15 @@ function validSpacing(s) {
     if(result == false) break;
     } return result;
  }
+
+/*You will be given an array of objects representing data about developers who have signed up to attend the coding meetup
+that you are organising for the first time. Your task is to return the number of JavaScript developers coming from Europe. */
+function countDevelopers(list) {
+  let counter = 0;
+  for(let i = 0; i < list.length; i += 1){
+    if(list[i].continent == 'Europe' && list[i].language == 'JavaScript'){
+      counter += 1;
+    }
+  } return counter;
+}
+
