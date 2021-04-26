e85299e2a50e5a2361c8ba061734aa28c9493d75
Olga Mkrtchyan <olia.mst@gmail.com>
2021-04-26 17:30:32 +0300

    Update 7kyu4.js

--- 7kyu4.js
+++ 7kyu4.js
@@ -27,3 +27,18 @@ function factorial(n){
     counter *= i;
   } return counter;
 }
+
+/*Return the number (count) of vowels in the given string. We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.*/
+function getCount(str) {
+  let vowelsCount = 0;
+  let arrAll = str.split('');
+  let arrVowels = ['a', 'e', 'i', 'o', 'u'];
+  
+  for(let i = 0; i < arrVowels.length; i += 1){
+    for(let j = 0; j < arrAll.length; j += 1){
+      if(arrVowels[i] == arrAll[j]) vowelsCount += 1;
+    }
+  }
+  return vowelsCount;
+}
