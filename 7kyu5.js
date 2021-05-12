c99832408b2f4d2db3ec573c8c8902ef5e5b3b65
Olga Mkrtchyan <olia.mst@gmail.com>
2021-05-12 11:41:14 +0300

    Update 7kyu5.js

--- 7kyu5.js
+++ 7kyu5.js
@@ -49,4 +49,15 @@ function circleOfNumbers(n, firstNumber) {
   else return -(n/2 - firstNumber);  
 }
 
+/* You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup
+that you are organising.Your task is to return either:
+    true if all developers in the list code in the same language; or
+    false otherwise.*/
+function isSameLanguage(list) {
+  for(let i = 1; i < list.length; i += 1){
+    if(list[i].language != list[0].language) return false;
+  } return true;
+}
+
+
 
