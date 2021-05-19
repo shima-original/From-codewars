286bc8ac4b23b90a5bc96ef0f76bd5fe263672fa
Olga Mkrtchyan <olia.mst@gmail.com>
2021-05-19 13:20:19 +0300

    Update 7kyu7.js

--- 7kyu7.js
+++ 7kyu7.js
@@ -8,3 +8,24 @@ function maskify(cc) {
   } return arr.join('');
 }
   
+//Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+function alternateCase(s) { 
+  let result = [];
+  let arr = s.split('');
+  for (let i = 0; i < arr.length; i += 1){
+    if(arr[i].toUpperCase() == arr[i]){
+      result.push(arr[i].toLowerCase());
+    } else{
+      result.push(arr[i].toUpperCase());
+    }
+  }
+  return result.join('');
+}
+
+//Count the number of divisors of a positive integer n.
+function getDivisorsCnt(n){
+    let counter = 0;
+    for(let i = 0; i <= n; i += 1){
+      if(n % i == 0) counter += 1;
+    } return counter;
+}
