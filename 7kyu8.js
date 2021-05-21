59c9e4a7f02ae508b78d0d8530e0f8e6f1d7ae2c
Olga Mkrtchyan <olia.mst@gmail.com>
2021-05-21 15:00:48 +0300

    Update 7kyu8.js

--- 7kyu8.js
+++ 7kyu8.js
@@ -1,2 +1,23 @@
 /*Finish the solution so that it sorts the passed in array of numbers. 
-If the function passes in an empty array or null/nil value then it should return an empty array. */
+If the function passes in an empty array or null value then it should return an empty array. */
+function solution(nums){
+  if(nums === null) return [];
+  else if(nums.length === 0) return [];
+  else{
+    return nums.sort((a, b) => a - b);
+  }
+}
+
+/* This time no story, no theory. The examples below show you how to write function accum.
+accum("abcd") -> "A-Bb-Ccc-Dddd" */
+function accum(s) {
+	let arr = s.split('');
+  let result = [];
+  for(let i = 0; i < arr.length; i += 1) {
+    if(i == arr.length - 1) result.push(arr[i].toUpperCase() + arr[i].toLowerCase().repeat(i));
+    else {
+      result.push(arr[i].toUpperCase() + arr[i].toLowerCase().repeat(i) + '-');
+    }
+  } 
+  return result.join('');
+}
