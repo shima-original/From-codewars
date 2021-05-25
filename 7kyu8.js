91647ac6dfea0aaccc7299168ba9bf1c749a470b
Olga Mkrtchyan <olia.mst@gmail.com>
2021-05-25 22:09:21 +0300

    Update 7kyu8.js

--- 7kyu8.js
+++ 7kyu8.js
@@ -28,3 +28,24 @@ var maxRedigit = function(num) {
   return result = (num > 99 && num < 1000) ? parseInt(num.toString().split('').sort((a, b) => b - a).join('')) : null;
 };
 
+/*Input : an array of integers.
+Output : this array, but sorted in such a way that there are two wings:  the left wing with numbers decreasing, the right wing with numbers increasing.
+The two wings have the same length. If the length of the array is odd the wings are around the bottom, if the length is even the bottom is considered
+to be part of the right wing. Each integer l of the left wing must be greater or equal to its counterpart r in the right wing, 
+the difference l - r being as small as possible. In other words the right wing must be nearly as steep as the left wing.*/
+function makeValley(arr) {
+   
+    let left = [];
+    let right = [];
+  
+    arr.sort((a, b) => b - a);
+   
+    for(i = 0; i < (arr.length - 1); i += 2){
+      left.push(arr[i]);
+      right.unshift(arr[i + 1]);
+    } 
+  
+  if(arr.length % 2)right.unshift(arr[arr.length - 1]);
+  let result = left.concat(right);
+  return result;
+}
