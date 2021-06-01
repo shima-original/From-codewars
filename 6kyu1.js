0f169b703ccc4db5bf9409ec3771992755fe717e
Olga Mkrtchyan <olia.mst@gmail.com>
2021-06-01 11:28:35 +0300

    Create 6kyu1.js

---
+++ 6kyu1.js
@@ -0,0 +1,3 @@
+/*  Two integer numbers are added using the column addition method. When using this method, some additions of digits produce non-zero carries to the next positions.
+Your task is to calculate the number of non-zero carries that will occur while adding the given numbers.
+The numbers are added in base 10.*/
