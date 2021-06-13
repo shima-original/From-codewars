54e6543c30ef63461e300e6600adc9c1e9895905
Olga Mkrtchyan <olia.mst@gmail.com>
2021-06-13 22:38:39 +0300

    Update 6kyu1.js

--- 6kyu1.js
+++ 6kyu1.js
@@ -1,8 +1,43 @@
-/* We search non-negative integer numbers, with at most 3 digits, such as the sum of the cubes of their digits is the number itself; we will call them "cubic" numbers.
-These "cubic" numbers of at most 3 digits are "hidden" with other numbers and characters in a string.
-The task is to find, or not, the "cubic" numbers in the string and then to make the sum of these "cubic" numbers found in the string, if any, and to return a string.
- if "cubic" numbers number1, number2, ... were found.
-The numbers in the output are to be in the order in which they are encountered in the input string.
-If no cubic numbers are found return the string: `"Unlucky"``. */
+/*In a small restaurant there are A tables for one person and B tables for two persons.
+It it known that N groups of people come today, each consisting of one or two people.
+If a group consist of one person, it is seated at a vacant one-seater table. If there are none of them, it is seated at a vacant two-seater table. 
+If there are none of them, it is seated at a two-seater table occupied by single person. 
+If there are still none of them, the restaurant denies service to this group.
+If a group consist of two people, it is seated at a vacant two-seater table. If there are none of them, the restaurant denies service to this group.
+You are given a chronological order of groups coming. You are to determine the total number of people the restaurant denies service to.
+Input contains two integers A and B - the number of one-seater and the number of two-seater tables respectively, 
+and a list of integers - the number of people in each group of clients in chronological order of their arrival.
+Return the total number of people the restaurant denies service to.*/
+function restaurant(a,b,t){
+  let result = 0;
+  let c = 0;
+  for(i = 0; i < t.length; i += 1){
+    
+    if(t[i] == 1){
+      if(a > 0){
+        a -= 1;
+      }
+      else if(a == 0 && b > 0){
+        b -= 1;
+        c += 1;
+      }
+      else if(a == 0 && b == 0 && c > 0){
+        c -= 1;
+      }
+      else if(a == 0 && b == 0){
+        result += 1;
+      }
+    }
+    
+    else if(t[i] == 2){
+      if(b > 0){
+        b -= 1;
+      }
+      else if(b == 0){
+        result += 2;
+      }
+    }
+  } return result;
+}
 
 
