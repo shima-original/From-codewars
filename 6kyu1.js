6493cf1e2efe9dd193a85fa19a3960fe3353a2e9
Olga Mkrtchyan <olia.mst@gmail.com>
2021-09-20 22:21:08 +0300

    Update 6kyu1.js

--- 6kyu1.js
+++ 6kyu1.js
@@ -85,4 +85,20 @@ function digPow(n, p){
   return result % n == 0 ? result/n : -1;
 }
 
+/*Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
+Rules for a smiling face:
+    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
+    A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
+    Every smiling face must have a smiling mouth that should be marked with either ) or D
+No additional characters are allowed except for those mentioned */
+function countSmileys(arr) {
+  let regexp = /^[:;][-~]?[)D]$/; 
+  let result = [];
+  for(let i = 0; i < arr.length; i += 1){
+    if(regexp.test(arr[i])) result.push(arr[i]);
+  }   
+  if(result === null) return 0;
+  return result.length;
+}
+
 
