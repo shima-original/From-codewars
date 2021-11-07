6b50222731e773d0c5bcc4beee7833e01372eaa9
Olga Mkrtchyan <olia.mst@gmail.com>
2021-11-07 18:53:50 +0300

    Create 6kyu4.js

---
+++ 6kyu4.js
@@ -0,0 +1,2 @@
+/*Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. 
+You can guarantee that input is non-negative.*/
