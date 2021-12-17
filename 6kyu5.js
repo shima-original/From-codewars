97f250443a130cbfc6510fd03a8986988f16fb79
Olga Mkrtchyan <olia.mst@gmail.com>
2021-12-17 21:09:07 +0300

    Update 6kyu5.js

--- 6kyu5.js
+++ 6kyu5.js
@@ -1,14 +1,14 @@
 /* Each exclamation mark's weight is 2; each question mark's weight is 3. Putting two strings left and right on the balance - are they balanced?
 If the left side is more heavy, return "Left"; if the right side is more heavy, return "Right"; if they are balanced, return "Balance". */
 function balance(left,right){
-  left.replace(/\!/g, '2');
-  left.replace(/\?/g, '3');
-  right.replace(/\!/g, '2');
-  right.replace(/\?/g, '3');  
-  let leftRes = left.split('').reduce(function(sum, current){
+ let replaced1 = left.replace(/\!/g, '2');
+ let replaced2 = replaced1.replace(/\?/g, '3');
+ let rightrepl1 = right.replace(/\!/g, '2');
+ let rightrepl2 = rightrepl1.replace(/\?/g, '3');  
+  let leftRes = replaced2.split('').reduce(function(sum, current){
         return (sum + parseInt(current, 10));
     }, 0);
-  let rightRes = right.split('').reduce(function(sum, current){
+  let rightRes = rightrepl2.split('').reduce(function(sum, current){
         return (sum + parseInt(current, 10));
     }, 0);  
   if (leftRes > rightRes) return "Left";
