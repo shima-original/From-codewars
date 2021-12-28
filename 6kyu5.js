90ca013e6756171fb862fadf8e24642a4cd4f28b
Olga Mkrtchyan <olia.mst@gmail.com>
2021-12-28 20:21:52 +0300

    Update 6kyu5.js

--- 6kyu5.js
+++ 6kyu5.js
@@ -44,3 +44,19 @@ function shorterestTime(n,m,speeds) {
     Math.abs(n - m) * a + 2 * b + c + n * a
   );
 }
+
+/*Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute 
+from the gloves you have in your drawer.A pair of gloves is constituted of two gloves of the same color.
+You are given an array containing the color of each glove.You must return the number of pair you can constitute.You must not change the input array.*/
+function numberOfPairs(gloves){
+  let counter = 0;
+  let resultArr = []; 
+  for(let i = 0; i < gloves.length; i += 1){
+    if(!resultArr.includes(gloves[i])) resultArr.push(gloves[i]);
+  }
+    for(let i = 0; i < resultArr.length; i += 1){
+         let lengthOfGloves = gloves.filter(item => item === gloves[i]).length;
+         if(lengthOfGloves % 2 === 0) counter += 1;
+    }
+  return counter;
+}
