8d5d93a9c52ab8920906ecc369fb8c3d34fad522
Olga Mkrtchyan <olia.mst@gmail.com>
2022-02-02 01:27:23 +0300

    Update 6kyu5.js

--- 6kyu5.js
+++ 6kyu5.js
@@ -88,3 +88,16 @@ function meeting(x, need){
     if(counter < need) return 'Not enough!';
   }
 }   
+
+/*Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers. */
+function order(words){
+  let result = Array(words.length).fill(0);
+  let array = words.split(' ');
+    for(let i = 0; i < array.length; i += 1){
+      let index = array[i].match(/\d/);
+      result.splice((index - 1), 0, array[i]);
+    }  
+  return result.filter(elem => elem != '0').join(' ');
+}
