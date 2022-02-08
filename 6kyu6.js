7dc858b2a52de935a9d62be4097006e15a203252
Olga Mkrtchyan <olia.mst@gmail.com>
2022-02-08 19:27:15 +0300

    Update 6kyu6.js

--- 6kyu6.js
+++ 6kyu6.js
@@ -1,1 +1,30 @@
 
+/* You will be given an array of objects representing data about developers who have signed up to attend the next web development meetup that you are organising. 
+Three programming languages will be represented: Python, Ruby and JavaScript.
+Your task is to return either:
+    true if the number of meetup participants representing any of the three programming languages is ** at most 2 times higher than the number of developers representing any of the remaining programming languages**; or
+    false otherwise.*/
+
+function isLanguageDiverse(list) {
+  let pcounter = 0;
+  let rcounter = 0;
+  let jcounter = 0;
+  
+  for(let i = 0; i < list.length; i += 1){
+    if(list[i].language.toLowerCase() === 'python'){
+      pcounter += 1;
+    }
+    else if(list[i].language.toLowerCase() === 'ruby'){
+      rcounter += 1;
+    }
+    else{
+      jcounter += 1;
+    }
+  }
+  
+  let max = Math.max(pcounter, rcounter, jcounter);
+  if(max/pcounter <= 2 && max/rcounter <= 2 && max/jcounter <= 2) return true;
+  else {
+    return false;
+  }
+}
