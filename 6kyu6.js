9203b392de6f6b0a855547784e910a004eceb05a
Olga Mkrtchyan <olia.mst@gmail.com>
2022-02-13 01:52:25 +0300

    Update 6kyu6.js

--- 6kyu6.js
+++ 6kyu6.js
@@ -28,3 +28,18 @@ function isLanguageDiverse(list) {
     return false;
   }
 }
+
+/*A stream of data is received and needs to be reversed.
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed
+The total number of bits will always be a multiple of 8.
+The data is given in an array*/
+
+function dataReverse(data) {
+  let result = [];
+  
+  while(data.length != 0){
+    let byte = data.splice(0, 8);
+    result.unshift(byte);
+  }
+  return result.flat();
+}
