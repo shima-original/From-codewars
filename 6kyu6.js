70a239ce066dc8961111249f83969bf2cbd065c0
Olga Mkrtchyan <olia.mst@gmail.com>
2022-02-15 00:17:59 +0300

    Update 6kyu6.js

--- 6kyu6.js
+++ 6kyu6.js
@@ -43,3 +43,15 @@ function dataReverse(data) {
   }
   return result.flat();
 }
+
+
+/* An ATM ran out of 10 dollar bills and only has 100, 50 and 20 dollar bills.Given an amount between 40 and 10000 dollars (inclusive) and assuming that the ATM 
+wants to use as few bills as possible, determinate the minimal number of 100, 50 and 20 dollar bills the ATM needs to dispense (in that order).*/
+
+function withdraw(n) {
+  let result = [];
+  result.push(n/100 >> 0);
+  result.push((n%100)/50 >> 0);
+  result.push(((n%100)%50)/20 >> 0); 
+  return result;
+}
