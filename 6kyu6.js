ecbdb803c80e9fe18b9f4943b51f8299e2202e28
Olga Mkrtchyan <olia.mst@gmail.com>
2022-05-11 00:12:34 +0300

    Update 6kyu6.js

--- 6kyu6.js
+++ 6kyu6.js
@@ -58,5 +58,27 @@ function withdraw(n) {
   }
 }
 
+/*Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], 
+drop the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which leads to [1,2,3,1,2,3].
+With list [20,37,20,21] and number 1, the result would be [20,37,21]. */
 
+function deleteNth(arr,n){
+  let newArr = [];
+  let result = {};
+  newArr = arr.map(function(a){
+    if (result[a] < n && result[a] > 0){
+      result[a] += 1;
+      return a;
+    }       
+    else if(result[a] === undefined){
+      result[a] = 1;
+      return a;
+    }
+    else {
+     result[a] += 1; 
+    }
+   });
+  return newArr.filter((elem) => elem != undefined);
+}
 
