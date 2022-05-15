975a18826f87f3dac2d3bd984cdd253421a6b6a7
Olga Mkrtchyan <olia.mst@gmail.com>
2022-05-15 18:55:40 +0300

    Update 6kyu6.js

--- 6kyu6.js
+++ 6kyu6.js
@@ -82,3 +82,25 @@ function deleteNth(arr,n){
   return newArr.filter((elem) => elem != undefined);
 }
 
+/* Your task is to write a function which cuts cancer cells from the body.
+Cancer cells are divided into two types:
+
+    Advance stage,described as letter C
+    Initial stage,described as letter c
+
+Rest cells are divided as follows:
+
+    Normal cell,described as lowercase letter
+    Important cell,described as uppercase letter
+
+Prerequisites:
+
+    Important cell,cannot be cut out.
+    Advance cancer cell,should be cut out with adjacent cells if it can be done.
+
+Function input is a string (representing a body), remove "cancer" characters (based on the described rules) 
+and return the body cured of those "cancer" characters.*/
+
+function cutCancerCells(organism){
+  return organism.replace(/c|[a-z]?C[a-z]?/g, '');
+}
