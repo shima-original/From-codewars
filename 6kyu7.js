674c74eca08a6f9339e6b70b45204897a84bbeca
Olga Mkrtchyan <olia.mst@gmail.com>
2022-06-12 13:10:03 +0300

    Create 6kyu7.js

---
+++ 6kyu7.js
@@ -0,0 +1,10 @@
+/*Given two arrays of strings a1 and a2 return a sorted array r in lexicographical order of the strings of a1 which are substrings of strings of a2.
+Notes:
+    Arrays are written in "general" notation. See "Your Test Cases" for examples in your language.
+    In Shell bash a1 and a2 are strings. The return is a string where words are separated by commas.
+    Beware: r must be without duplicates. */
+
+function inArray(array1,array2){ 
+  return array1.filter(elemA1 => (array2.find(elemA2 => elemA2.match(elemA1)))).sort();  
+}
+
