2d54427976c56dd54cb25253830527eab92ec76d
Olga Mkrtchyan <olia.mst@gmail.com>
2022-07-03 12:51:58 +0300

    Update 6kyu8.js

--- 6kyu8.js
+++ 6kyu8.js
@@ -21,3 +21,24 @@ function getLengthOfMissingArray(arrayOfArrays) {
 }
 
 
+/* Training on Mexical Wave.
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. 
+You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+If the character in the string is whitespace then pass over it as if it was an empty seat*/
+
+function wave(str){
+  let result = [];
+  let arr = str.split('');
+  
+  for(let i = 0; i < arr.length; i += 1){
+    if(arr[i] != ' '){
+        arr[i] = arr[i].toUpperCase();
+        result.push(arr.join(''));
+        arr[i] = arr[i].toLowerCase();
+    } 
+    else{
+      continue;
+    }
+  }
+  return result;
+}
