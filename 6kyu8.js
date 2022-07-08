cc404175391f2990f7974aa9801e733a652a1247
Olga Mkrtchyan <olia.mst@gmail.com>
2022-07-09 00:38:29 +0300

    Update 6kyu8.js

--- 6kyu8.js
+++ 6kyu8.js
@@ -42,3 +42,7 @@ function wave(str){
   }
   return result;
 }
+
+/* A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. 
+In this Kata, we will restrict ourselves to decimal (base 10).
+Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.*/
