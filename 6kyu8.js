e4c3f03a200af9c44eba19fbd289112855324329
Olga Mkrtchyan <olia.mst@gmail.com>
2022-07-09 00:39:50 +0300

    Update 6kyu8.js

--- 6kyu8.js
+++ 6kyu8.js
@@ -46,3 +46,14 @@ function wave(str){
 /* A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. 
 In this Kata, we will restrict ourselves to decimal (base 10).
 Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.*/
+
+function narcissistic(value) {
+  let array = [];
+  for(let i = 0; i < value; i += 1){
+    array.unshift(value % 10);
+    value = Math.floor(value/10);
+  }
+  for(let i = 0; i < array.length; i += 1){
+    
+  }
+}
