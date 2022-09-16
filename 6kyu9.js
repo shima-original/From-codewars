f1757e2e6998d6adf531ca7b2703a6b29b6b523a
Olga Mkrtchyan <olia.mst@gmail.com>
2022-09-16 22:51:10 +0300

    Update 6kyu9.js

--- 6kyu9.js
+++ 6kyu9.js
@@ -26,3 +26,25 @@ function zeroPlentiful(arr){
         }
   return counter;
 }
+
+/* Each time they go on a mission, each Mormon converts a fixed number of people (reach) into followers. 
+This continues and every freshly converted Mormon as well as every original Mormon go on another mission and convert the same fixed number of people each. 
+The process continues until they reach a predefined target number of followers (target).
+Converted Mormons are unique so that there's no duplication amongst them.
+Complete the function that calculates how many missions Mormons need to embark on, in order to reach their target. 
+While each correct solution will pass, for more fun try to make a recursive function.
+All inputs are valid positive integers.*/
+
+function Mormons(startingNumber, reach, target) {
+    let number = startingNumber;
+    let counter = 0;
+    function newMormons(counter, number, reach, target){
+      if(number >= target) return counter;
+      else{
+        counter += 1;
+        number += (number * reach);
+        return newMormons(counter, number, reach, target);
+      }
+    }
+    return newMormons(counter, number, reach, target); 
+}
