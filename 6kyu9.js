2d2243ea54dabacc25a93395266c29e7f00a9e3e
Olga Mkrtchyan <olia.mst@gmail.com>
2022-09-23 22:33:18 +0300

    Update 6kyu9.js

--- 6kyu9.js
+++ 6kyu9.js
@@ -89,3 +89,21 @@ function calculate1RM(w, r){
     return Math.max(result1, result2, result3);
   }
 }
+
+/* The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}.*/
+
+function count (string) {  
+  
+  let resultObj = {};
+  let arr = string.split('');
+  
+  for(let i = 0; i < arr.length; i += 1){
+    if(Object.prototype.hasOwnProperty.call(resultObj, arr[i])){
+      resultObj[arr[i]] += 1;
+    } else{
+        resultObj[arr[i]] = 1;
+    }
+  }
+  return resultObj;
+}
