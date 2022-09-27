b52ea3c76f4fb99d5f4c044e02ad14c8eadab55d
Olga Mkrtchyan <olia.mst@gmail.com>
2022-09-27 19:22:54 +0300

    Update 6kyu10.js

--- 6kyu10.js
+++ 6kyu10.js
@@ -1,1 +1,12 @@
 
+/* If a < b and b < c then by transitivity we have a < c, right?
+Well not in JavaScript. Find three values which give a nice transitive cycle!
+Your method transitiveCycle() should return an array of 3 elements [a, b, c] such that
+a < b && b < c && c < a === true
+Also your elements should be equal to itself. */
+
+function transitiveCycle() {
+  return ['7', 9, '13'];
+}
+
+
