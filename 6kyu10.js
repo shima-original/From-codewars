eee18a5b038cdb0e205ac8bdc30e94d0827b6cb2
Olga Mkrtchyan <olia.mst@gmail.com>
2022-09-30 22:51:11 +0300

    Update 6kyu10.js

--- 6kyu10.js
+++ 6kyu10.js
@@ -40,3 +40,24 @@ function chained(functions) {
   }
 }
 
+
+/* Pirates have notorious difficulty with enunciating. They tend to blur all the letters together and scream at people.
+At long last, we need a way to unscramble what these pirates are saying.
+Write a function that will accept a jumble of letters as well as a dictionary, and output a list of words that the pirate might have meant.
+For example:
+grabscrab( "ortsp", ["sport", "parrot", "ports", "matey"] )
+Should return ["sport", "ports"].
+Return matches in the same order as in the dictionary. Return an empty array if there are no matches. */
+
+function grabscrab(anagram, dictionary) {
+  let result = [];
+  let sortedAnagram = anagram.split('').sort().join('');
+  
+  //let sortedDictionary = dictionary.forEach(elem => elem.split('').sort().join('')).filter(elem => elem === sortedAnagram);
+  //return sortedDictionary;
+  
+  for(let i = 0; i < dictionary.length; i += 1){
+    if(dictionary[i].split('').sort().join('') === sortedAnagram) result.push(dictionary[i]);
+  }
+  return result;
+}
