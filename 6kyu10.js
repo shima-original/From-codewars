71cc0d918186b59d69275d26fc2b6f4a17b375b7
Olga Mkrtchyan <olia.mst@gmail.com>
2022-09-30 21:21:48 +0300

    Update 6kyu10.js

--- 6kyu10.js
+++ 6kyu10.js
@@ -25,3 +25,18 @@ function ascendDescend(length, minimum, maximum) {
   return result.repeat(length).slice(0, length);
 }
 
+
+/* Your task is to write a higher order function for chaining together a list of unary functions. 
+In other words, it should return a function that does a left fold on the given functions.
+chained([a,b,c,d])(input)
+Should yield the same result as
+d(c(b(a(input)))) */
+
+function chained(functions) {
+  return function(x){
+    for(let i = 0; i < functions.length; i += 1){
+      x = functions[i](x);
+    } return x;
+  }
+}
+
