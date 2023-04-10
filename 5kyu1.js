8f9ce6ef8c636763add9321aa3de39dca96703f2
Olga Mkrtchyan <olia.mst@gmail.com>
2023-04-10 22:37:32 +0300

    Update 5kyu1.js

--- 5kyu1.js
+++ 5kyu1.js
@@ -100,3 +100,33 @@ function times(a) {
 function dividedBy(a) {
   return b => Math.floor(b / a);
 }
+
+
+/*John and Mary want to travel between a few towns A, B, C ... Mary has on a sheet of paper a list of distances between these towns. ls = [50, 55, 57, 58, 60]. John is tired of driving and he says to Mary that he doesn't want to drive more than t = 174 miles and he will visit only 3 towns.
+
+Which distances, hence which towns, they will choose so that the sum of the distances is the biggest possible to please Mary and John? */
+
+function chooseBestSum(t, k, ls) {
+ 
+  let biggest = 0;
+  
+  let recurseTowns = function(towns, lastIndex){
+    
+    towns = towns || [];
+    
+    if(towns.length === k){
+      let sum = towns.reduce((a, b) => a + b);
+      if(sum > biggest && sum <= t){
+        biggest = sum;
+      }
+      return;
+    } 
+   
+    for(let i = lastIndex + 1 || 0; i <= ls.length; i += 1){
+      recurseTowns(towns.concat(ls[i]), i);
+    }
+    
+   }
+    recurseTowns();
+    return biggest || null;
+}
